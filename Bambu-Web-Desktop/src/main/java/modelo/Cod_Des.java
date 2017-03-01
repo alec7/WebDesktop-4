70ce2946615091f5d69cc49d30f6076b716c64ac
gerardoco2 <gerardocolina@gmail.com>
2017-02-28 21:55:01 -0430

    agregue el modelo cod_des

---
+++ Bambu-Web-Desktop/src/main/java/modelo/Cod_Des.java
@@ -0,0 +1,35 @@
+package modelo;
+
+public class Cod_Des 
+
+{
+	
+	String codigo, descripcion;
+
+	public Cod_Des(String codigo, String descripcion) {
+		super();
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
+	
+	
+
+}
